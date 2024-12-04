e130d1b6c2f9bc397d7a2272a6c2e083f24ff847
Red_Robles <redworld22@hotmail.com>
2024-12-04 11:44:50 -0400

    Sección de películas en tendencias (TrendingPreviewMoviesContainer)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+async function getTrendingPreviewMoviesContainer(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    
+    const movies = data.results
+    console.log({data, movies});
+
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
+
+        const divImg = document.createElement('div');
+        divImg.classList.add('movie-container');
+
+        divImg.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(divImg)
+    });
+}
+
+getTrendingPreviewMoviesContainer()
